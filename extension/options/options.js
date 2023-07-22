20f97d7b0e34b2811a4cc3e4d2fa1fb5d25c6cef
andrewpr52 <andrewpr52@gmail.com>
2023-07-21 22:03:06 -0500

    removed unnecessary code from options.js

--- extension/options/options.js
+++ extension/options/options.js
@@ -1,37 +1,9 @@
-// Saves options to chrome.storage
-const saveOptions = () => {
-    const playerModalEnabled = document.getElementById('playerModalEnabled').checked;
-    const matchupPlayersEnabled = document.getElementById('matchupPlayersEnabled').checked;
-    const teamPanelEnabled = document.getElementById('teamPanelEnabled').checked;
-    const trendPanelEnabled = document.getElementById('trendPanelEnabled').checked;
-    const tradesPanelEnabled = document.getElementById('tradesPanelEnabled').checked;
-    const leagueChatEnabled = document.getElementById('leagueChatEnabled').checked;
-
-    chrome.storage.sync.set(
-        { playerModalEnabled, matchupPlayersEnabled, teamPanelEnabled, trendPanelEnabled, tradesPanelEnabled, leagueChatEnabled },
-        () => {
-            // Update status to let user know options were saved.
-            const status = document.getElementById('status');
-            status.textContent = 'Options saved.';
-            
-            setTimeout(() => {
-                status.textContent = '';
-            }, 3000);
-        }
-    );
-};
-
 document.querySelectorAll("input[type='checkbox']").forEach(checkbox => {
     checkbox.addEventListener('change', (e) => {
-        saveOption(e.target.id, e.target.checked);
+        chrome.storage.sync.set({ [e.target.id]: e.target.checked} );
     });
 });
 
-const saveOption = (key, value) => {
-    chrome.storage.sync.set({ [key]: value} );
-};
-
-// Restores select box and checkbox state using the preferences stored in chrome.storage.
 const restoreOptions = () => {
     chrome.storage.sync.get(
         { playerModalEnabled: true, matchupPlayersEnabled: true, teamPanelEnabled: true, trendPanelEnabled: true, tradesPanelEnabled: true, leagueChatEnabled: true },
